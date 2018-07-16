6730453e00408282b333c9e63ebb6d2bda55908b
Natalia Nazaruk <natalia.nazaruk@assecods.pl>
2018-07-16 14:31:14 +0200

    Intent opening PlacesActivity added

--- app/src/main/java/pl/nataliana/foreignersinbydgoszcz/activities/MainActivity.java
+++ app/src/main/java/pl/nataliana/foreignersinbydgoszcz/activities/MainActivity.java
@@ -1,7 +1,9 @@
 package pl.nataliana.foreignersinbydgoszcz.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import pl.nataliana.foreignersinbydgoszcz.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void seePlaces(View view) {
+        Intent c = new Intent(MainActivity.this, PlacesActivity.class);
+        startActivity(c);
     }
 }
