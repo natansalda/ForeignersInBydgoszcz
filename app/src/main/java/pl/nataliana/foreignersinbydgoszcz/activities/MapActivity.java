ed9d8c9c6afb73c8d22fea9c6931584a63a59a66
Natalia Nazaruk <natalia.nazaruk@assecods.pl>
2018-07-18 14:29:38 +0200

    MapsActivity created

---
+++ app/src/main/java/pl/nataliana/foreignersinbydgoszcz/activities/MapActivity.java
@@ -0,0 +1,37 @@
+package pl.nataliana.foreignersinbydgoszcz.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+
+import pl.nataliana.foreignersinbydgoszcz.R;
+
+public class MapActivity extends Activity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        LatLng sydney = new LatLng(-33.867, 151.206);
+
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+
+        map.addMarker(new MarkerOptions()
+                .title("Sydney")
+                .snippet("The most populous city in Australia.")
+                .position(sydney));
+    }
+}
+
+
