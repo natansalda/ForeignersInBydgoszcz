1d42fe2a66433cfafae227712bf45b0e30ccd848
Natalia Nazaruk <nazarjazz@wp.pl>
2018-08-08 22:17:46 +0200

    Some changes to widget

--- app/src/main/java/pl/nataliana/foreignersinbydgoszcz/widget/WidgetProvider.java
+++ app/src/main/java/pl/nataliana/foreignersinbydgoszcz/widget/WidgetProvider.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.widget.RemoteViews;
 
 import pl.nataliana.foreignersinbydgoszcz.R;
@@ -13,29 +14,54 @@ import pl.nataliana.foreignersinbydgoszcz.model.Task;
 
 public class WidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                Task task, int appWidgetId) {
+//    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+//                                     Task task, int appWidgetId) {
 
-        Intent intent = TasksActivity.myIntent(context, task);
+//        Intent intent = TasksActivity.myIntent(context, task);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
+//                PendingIntent.FLAG_UPDATE_CURRENT);
+//
+//        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+//        views.removeAllViews(R.id.widget_list_view);
+//        views.setOnClickPendingIntent(R.id.task_widget_holder, pendingIntent);
+
+//        Intent listViewIntent = new Intent(context, WidgetService.class);
+//        listViewIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
+//        listViewIntent.setData(Uri.parse(listViewIntent.toUri(Intent.URI_INTENT_SCHEME)));
+//        RemoteViews widget = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+//        widget.setRemoteAdapter(R.id.widget_list_view, listViewIntent);
+
+//        appWidgetManager.updateAppWidget(appWidgetId, views);
+//        appWidgetManager.updateAppWidget(appWidgetId, widget);
+//    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+        Intent intent = new Intent(context, TasksActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_UPDATE_CURRENT);
 
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-        views.removeAllViews(R.id.widget_list_view);
+        //views.removeAllViews(R.id.widget_list_view);
         views.setOnClickPendingIntent(R.id.task_widget_holder, pendingIntent);
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     public static void updateTaskWidgets(Context context, AppWidgetManager appWidgetManager,
-                                         Task recipe, int[] appWidgetIds) {
+                                         Task task, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widget_list_view);
-            updateAppWidget(context, appWidgetManager, recipe, appWidgetId);
+            updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 }
